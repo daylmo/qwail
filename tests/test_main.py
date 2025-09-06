2f86506f87c3562bb5241a923e05a14768c05d38
Dayal Moirangthem <daylmo@proton.me>
2025-09-06 18:56:41 +0530

    wip

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,11 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
 def test_main():
-    assert True
+    r = client.get("/")
+    assert r.status_code == status.HTTP_200_OK
