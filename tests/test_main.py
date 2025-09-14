5d0aa32dbce17ce6ab5ad4e6fa547c8920db71d3
Dayal Moirangthem <daylmo@proton.me>
2025-09-14 09:32:40 +0530

    test: update `test_main.py`

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,13 @@
-def test_dummy():
-    assert True
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from qwail.main import app
+
+client = TestClient(app=app)
+
+
+@pytest.mark.asyncio
+async def test_main():
+    r = client.get("/")
+    assert r.status_code == status.HTTP_200_OK
